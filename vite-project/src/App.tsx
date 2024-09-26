78bc8eff1779b2b44d5dfc75d53320fca9f08da2
himani1001 <himanisharma15435029@gmail.com>
2024-09-26 22:23:06 +0530

    Passing children

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,28 +1,33 @@
-import TodoList from "./components/TodoList";
+// import TodoList from "./components/TodoList";
+
+import Alert from "./components/Alert";
 
 function App(){ 
 
-  let days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-]
+//   let days = [
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday',
+//     'Sunday'
+// ]
 
-  const handleSelectItem = (day: string) => {
-    console.log(day);
-  }
+//   const handleSelectItem = (day: string) => {
+//     console.log(day);
+//   }
 
   return (
   <div>
-    <TodoList days = {days} heading = 'Days' onSelectItem={
+    {/* <TodoList days = {days} heading = 'Days' onSelectItem={
       //can write an inline function or separate event handler
-      handleSelectItem}/>
+      handleSelectItem}/> */}
+
+      <Alert>
+        Hello <span>World!</span>
+      </Alert>
     </div>
   )
 }
-
 export default App;
